0774c0bbd98a1d08ea11d54e2328da9c8da380c0
Shadab Shah <85732713+shadabshah1680@users.noreply.github.com>
2023-08-05 17:00:19 +0500

    Create db.js

---
+++ utils/db.js
@@ -0,0 +1,11 @@
+// utils/db.js
+import mysql from 'mysql';
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+// Rest of the code remains the same
